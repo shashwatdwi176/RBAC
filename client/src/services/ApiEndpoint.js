a97a08fecc33bdcf49985a435430aa4c5a1a10d9
Shashwat Dwivedi <121190747+shashwatdwi176@users.noreply.github.com>
2024-11-27 23:26:40 +0530

    Update ApiEndpoint.js

--- client/src/services/ApiEndpoint.js
+++ client/src/services/ApiEndpoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'https://role-based-access-control-backend.onrender.com/',
+    baseURL:'http://localhost:4000',
     headers:{
         'Content-Type': 'application/json'
     },
